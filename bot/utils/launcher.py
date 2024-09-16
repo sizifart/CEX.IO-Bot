f9bdddfee133d54bd3fb59b950720de0f84ed575
Foad Davoodi <myf.davoodi@gmail.com>
2024-09-16 22:38:36 +0330

    Add files via upload

---
+++ bot/utils/launcher.py
@@ -0,0 +1,142 @@
+import os
+import glob
+import asyncio
+import argparse
+from itertools import cycle
+
+from pyrogram import Client
+from better_proxy import Proxy
+
+from bot.config import settings
+from bot.utils import logger
+from bot.core.tapper import run_tapper
+from bot.core.registrator import register_sessions
+from bot.utils.version_updater import parser
+from bot.core.headers import headers
+
+start_text = """
+
+ __   ___        __      __   __  ___ 
+/  ` |__  \_/ | /  \    |__) /  \  |  
+\__, |___ / \ | \__/    |__) \__/  |  
+
+            BY VANHBAKA                                                                                                       
+
+Select an action:
+
+    1. Run clicker
+    2. Create session
+"""
+
+global tg_clients
+
+
+def get_app_version():
+    version = parser.x_appl_version("https://cexp8.cex.io/static/js/main.a7314763.js")
+    if version:
+        parser.save_version_to_file(version)
+    else:
+        logger.info("Could not find 'const g' in the content.")
+
+
+def get_session_names() -> list[str]:
+    session_names = sorted(glob.glob("sessions/*.session"))
+    session_names = [
+        os.path.splitext(os.path.basename(file))[0] for file in session_names
+    ]
+
+    return session_names
+
+
+def get_proxies() -> list[Proxy]:
+    if settings.USE_PROXY_FROM_FILE:
+        with open(file="bot/config/proxies.txt", encoding="utf-8-sig") as file:
+            proxies = [Proxy.from_str(proxy=row.strip()).as_url for row in file]
+    else:
+        proxies = []
+
+    return proxies
+
+
+async def get_tg_clients() -> list[Client]:
+    global tg_clients
+
+    session_names = get_session_names()
+
+    if not session_names:
+        raise FileNotFoundError("Not found session files")
+
+    if not settings.API_ID or not settings.API_HASH:
+        raise ValueError("API_ID and API_HASH not found in the .env file.")
+
+    tg_clients = [
+        Client(
+            name=session_name,
+            api_id=settings.API_ID,
+            api_hash=settings.API_HASH,
+            workdir="sessions/",
+            plugins=dict(root="bot/plugins"),
+        )
+        for session_name in session_names
+    ]
+
+    return tg_clients
+
+async def auto_update_version():
+    while True:
+        await asyncio.sleep(3600)
+        get_app_version()
+        with open("x-appl-version.txt", "r") as f:
+            version = f.read()
+            headers['x-appl-version'] = str(version)
+
+
+
+async def process() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--action", type=int, help="Action to perform")
+
+    logger.info(f"Detected {len(get_session_names())} sessions | {len(get_proxies())} proxies")
+    get_app_version()
+
+    action = parser.parse_args().action
+
+    if not action:
+        print(start_text)
+
+        while True:
+            action = input("> ")
+
+            if not action.isdigit():
+                logger.warning("Action must be number")
+            elif action not in ["1", "2"]:
+                logger.warning("Action must be 1 or 2")
+            else:
+                action = int(action)
+                break
+
+    if action == 2:
+        await register_sessions()
+    elif action == 1:
+        tg_clients = await get_tg_clients()
+
+        await run_tasks(tg_clients=tg_clients)
+
+
+async def run_tasks(tg_clients: list[Client]):
+    proxies = get_proxies()
+    proxies_cycle = cycle(proxies) if proxies else None
+    with open("x-appl-version.txt", "r") as f:
+        version = f.read()
+    tasks = [
+        asyncio.create_task(
+            run_tapper(
+                tg_client=tg_client,
+                proxy=next(proxies_cycle) if proxies_cycle else None,
+                app_version=str(version),
+            )
+        )
+        for tg_client in tg_clients
+    ]
+    tasks.append(asyncio.create_task(auto_update_version()))
+    await asyncio.gather(*tasks)
---
+++ bot/utils/version_updater/parser.py
@@ -0,0 +1,74 @@
+import requests
+import re
+import os
+from bot.utils import logger
+
+
+def get_main_js_format(base_url):
+    try:
+        response = requests.get(base_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        content = response.text
+        matches = re.findall(r'(/static/js/main\.[^.]+\.js)', content)
+        if matches:
+            # Return all matches, sorted by length (assuming longer is more specific)
+            return sorted(set(matches), key=len, reverse=True)
+        else:
+            return None
+    except requests.RequestException as e:
+        logger.warning(f"Error fetching the base URL: {e}")
+        return None
+
+
+def x_appl_version(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        content = response.text
+        match = re.search(r'const\s+g\s*=\s*"([^"]*)"', content)
+
+        if match:
+            return match.group(1)
+        else:
+            logger.info("Could not find 'const g' in the content.")
+            return None
+    except requests.RequestException as e:
+        logger.warning(f"Error fetching the JS file: {e}")
+        return None
+
+
+def save_version_to_file(version, filename="x-appl-version.txt"):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            existing_version_line = file.read().strip()
+
+        existing_version = re.search(r"'x-appl-version': '([^']*)'", existing_version_line)
+        if existing_version and existing_version.group(1) == version:
+            logger.info("Version is the same. No changes made.")
+            return
+
+    with open(filename, 'w') as file:
+        file.write(version)
+        logger.info(f"Version updated to {version} and saved to {filename}")
+
+def get_app_version():
+    base_url = "https://cexp.cex.io/"
+    main_js_formats = get_main_js_format(base_url)
+
+    if main_js_formats:
+        for format in main_js_formats:
+            logger.info(f"Trying format: {format}")
+            full_url = f"https://cexp.cex.io{format}"
+            result = x_appl_version(full_url)
+            if result:
+                save_version_to_file(result)
+                break
+        else:
+            logger.warning("Could not find 'const g' in any of the JS files.")
+    else:
+        logger.info("Could not find any main.js format. Dumping page content for inspection:")
+        try:
+            response = requests.get(base_url)
+            print(response.text[:1000])  # Print first 1000 characters of the page
+        except requests.RequestException as e:
+            logger.warning(f"Error fetching the base URL for content dump: {e}")
