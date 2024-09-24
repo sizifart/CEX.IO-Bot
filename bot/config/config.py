ba509e1cc4b404fc38a9d0cc81ca728f0745808f
Foad Davoodi <myf.davoodi@gmail.com>
2024-09-24 21:31:49 +0330

    Add files via upload

---
+++ bot/config/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    API_ID: int
+    API_HASH: str
+
+    REF_LINK: str = "https://t.me/cexio_tap_bot?start=1716374168523141"
+
+    AUTO_TAP: bool = True
+    RANDOM_TAPS_COUNT: list = [25, 75]
+    SLEEP_BETWEEN_TAPS: list = [25, 35]
+    SLEEP_BY_MIN_ENERGY: int = 250
+    AUTO_CONVERT: bool = True
+    MINIMUM_TO_CONVERT: float = 0.1
+    AUTO_BUY_UPGRADE: bool = True
+    WAIT_FOR_MOST_PROFITABLE_CARD: bool = True
+    AUTO_TASK: bool = True
+    AUTO_CLAIM_SQUAD_BONUS: bool = False
+
+    USE_PROXY_FROM_FILE: bool = False
+
+
+settings = Settings()
+
+
